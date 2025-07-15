db9861ce79deb167f1ddb27f804b5022081be66c
unknown <uayushi73@gmail.com>
2025-07-15 12:02:07 +0530

    added both

---
+++ LoggingMiddleware/auth.js
@@ -0,0 +1,28 @@
+// auth.js
+const axios = require('axios');
+
+async function getAuthToken() {
+  const AUTH_URL = 'http://20.244.56.144/evaluation-service/auth';
+
+  const credentials = {
+    email: "AYUSHIUNIYAL.220122355@gehu.ac.in",
+    name: "Ayushi Uniyal",
+    rollNo: "2218609",
+    accessCode: "QAhDUr",
+    clientID: "ecef3fe1-ffd8-4408-8d72-7563bfebf06c",
+    clientSecret: "dbEnGcqNCeYSNaHq"
+  };
+
+  try {
+    const response = await axios.post(AUTH_URL, credentials);
+    console.log("✅ Access Token Received:");
+    console.log({
+      token: response.data.access_token,
+      type: response.data.token_type
+    });
+  } catch (error) {
+    console.error("❌ AUTH FAILED:", error.response?.data || error.message);
+  }
+}
+
+getAuthToken();
---
+++ LoggingMiddleware/register.js
@@ -0,0 +1,28 @@
+// register.js
+const axios = require('axios');
+
+async function register() {
+  const REGISTER_URL = 'http://20.244.56.144/evaluation-service/register';
+  
+  const registrationData = {
+    email: "AYUSHIUNIYAL.220122355@gehu.ac.in",            
+    name: "Ayushi Uniyal",                
+    mobileNo: "8791989877",                
+    githubUsername: "Ayushii-uniyal",           
+    rollNo: "2218609",                     
+    accessCode: "QAhDUr"                   
+  };
+
+  try {
+    const response = await axios.post(REGISTER_URL, registrationData);
+    console.log("✅ REGISTRATION SUCCESS! Save these credentials:");
+    console.log({
+      clientID: response.data.clientID,
+      clientSecret: response.data.clientSecret
+    });
+  } catch (error) {
+    console.error(" REGISTRATION FAILED:", error.response?.data || error.message);
+  }
+}
+
+register();
